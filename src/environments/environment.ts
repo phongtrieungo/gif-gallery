8420333d00cb923670946b637e7065a04ed804b7
Phong Ngo <phong.trieungo@hotmail.com>
2022-09-24 17:01:34 +0700

    Adding API ey

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-
+export const API_KEY = 'yUwM3GFwj1XxCagiof8jFxKK3kcM3sT6';
 export const environment = {
   production: false
 };
@@ -13,4 +13,4 @@ export const environment = {
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
  */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
